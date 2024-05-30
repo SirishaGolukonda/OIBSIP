a9bfe1b77f0173a6f250fbe965807e4ca79458a5
SirishaGolukonda <ishagolukonda6@gmail.com>
2024-05-30 19:48:30 +0530

    Update bmicalculator.py

--- bmicalculator.py
+++ bmicalculator.py
@@ -1,17 +1,16 @@
 name=input("enter your name:")
-w=int(input("enter your weight in kgs:"))
-h=int(input("enter your height in cms:"))
-h_in_mt=h/100
-BMI=(w)/(h_in_mt**2)
-print(BMI)
+w=int(input("enter your weight in kg:"))
+h=int(input("enter your height in cm:"))
+BMI=(w)/(h/100)**2
+print("Your Body Mass Index is",BMI)
 if BMI>0:
-    if(BMI<18.5):
+    if(BMI<=18.5):
         print(name +",you are underweight.")
     elif(BMI<=24.9):
-        print(name +",your weight is normal.")
-    elif(BMI<29.9):
+        print(name +",your weight is normal and you are healthy.")
+    elif(BMI<=29.9):
         print(name +",you are overweight.")
-    elif(BMI<34.9):
+    elif(BMI<=30.0):
         print(name +",you are obese.")
     elif(BMI<39.9):
         print(name +",you are severly obese.")
